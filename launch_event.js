ad76b872f8a743ab7de6bbb6a6d16def6bb6ccf3
Nakul Aggarwal <nakulaggarwal98@gmail.com>
2023-05-02 17:31:24 +0530

    Updated launch event page

--- launch_event.js
+++ launch_event.js
@@ -12,7 +12,7 @@ window.onclick = function(event) {
     }
 }
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbwb98Sl_m_STXnGwWBlYPuvzJXndBsPOyWIyaypGqqM9-WDUsXaAYI2mIZ4ZWinhGQ/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwirPkr9jf7w4_pBeExvrjk5J1GuxYDZIwVwhJbfLaqdEGN0NVb4ODQ-9IIa3X6nCQ/exec'
 const form = document.forms['google-sheet']
 
 form.addEventListener('submit', e => {
@@ -29,7 +29,36 @@ form.addEventListener('submit', e => {
     .catch(error => console.error('Error!', error.message))
     .finally(() => {
         // Reset submit button to original state
-        submitBtn.innerHTML = 'Submit';
+        submitBtn.innerHTML = 'Confirm RSVP';
         submitBtn.classList.remove('disabled');
       });
 });
+
+
+// function handleFormSubmit(event) {
+//     event.preventDefault(); // Prevent the form from submitting normally
+//     var formData = new FormData(event.target);
+//     var email = formData.get('email'); // Get the value of the email field
+
+//     gapi.client.sheets.spreadsheets.values.get({
+//       spreadsheetId: 'YOUR_SPREADSHEET_ID',
+//       range: 'Sheet1!A:C',
+//       majorDimension: 'ROWS'
+//     }).then(function(response) {
+//       var values = response.result.values;
+//       for (var i = 0; i < values.length; i++) {
+//         if (values[i][0] == email) { // Match the email address
+//           var name = values[i][1];
+//           var message = values[i][2];
+//           console.log('Name:', name);
+//           console.log('Message:', message);
+//           // Do something with the data
+//         }
+//       }
+//     }, function(error) {
+//       console.error('Error reading data from Google Sheet', error);
+//     });
+//   }
+
+//   var form = document.getElementById('my-form');
+//   form.addEventListener('submit', handleFormSubmit);
