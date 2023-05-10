d01821578d3440c896664a5f3f7e48f00d23b009
Nakul Aggarwal <nakulaggarwal98@gmail.com>
2023-05-10 18:28:11 +0530

    Eula, privacy_policy and Terms conditions update

--- terms_condition.js
+++ terms_condition.js
@@ -1,65 +1,65 @@
-window.addEventListener('scroll', function() {
-    var scrollPos = window.scrollY;
-  
-    var sections = document.querySelectorAll("h3, h5");
-    var currentSectionIndex = 0;
-    for (var i = 0; i < sections.length; i++) {
-      var section = sections[i];
-      var sectionTop = section.offsetTop;
-      var sectionBottom = sectionTop + section.offsetHeight;
-      if (scrollPos >= sectionTop && scrollPos < sectionBottom) {
-        currentSectionIndex = i;
-      }
-    }
-  
-    var links = document.querySelectorAll('.sidenav a');
-    var activeLinkIndex = -1;
-    for (var i = 0; i < links.length; i++) {
-      var sectionId = links[i].getAttribute('href').replace('#', '');
-      var section = document.getElementById(sectionId);
-      var sectionRect = section.getBoundingClientRect();
-      if (sectionRect.top >= 0 && sectionRect.bottom <= window.innerHeight) {
-        activeLinkIndex = i;
-        break;
-      }
-    }
-  
-    for (var i = 0; i < links.length; i++) {
-      if (i === activeLinkIndex) {
-        links[i].classList.add('active');
-      } else {
-        links[i].classList.remove('active');
-      }
-    }
-  });
+window.addEventListener("scroll", function () {
+  var scrollPos = window.scrollY;
 
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const navigation = document.querySelector('.sidenav');
+  var sections = document.querySelectorAll("h3, h5");
+  var currentSectionIndex = 0;
+  for (var i = 0; i < sections.length; i++) {
+    var section = sections[i];
+    var sectionTop = section.offsetTop;
+    var sectionBottom = sectionTop + section.offsetHeight;
+    if (scrollPos >= sectionTop && scrollPos < sectionBottom) {
+      currentSectionIndex = i;
+    }
+  }
+
+  var links = document.querySelectorAll(".sidenav a");
+  var activeLinkIndex = -1;
+  for (var i = 0; i < links.length; i++) {
+    var sectionId = links[i].getAttribute("href").replace("#", "");
+    var section = document.getElementById(sectionId);
+    var sectionRect = section.getBoundingClientRect();
+    if (sectionRect.top >= 0 && sectionRect.bottom <= window.innerHeight) {
+      activeLinkIndex = i;
+      break;
+    }
+  }
+
+  for (var i = 0; i < links.length; i++) {
+    if (i === activeLinkIndex) {
+      links[i].classList.add("active");
+    } else {
+      links[i].classList.remove("active");
+    }
+  }
+});
+
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const navigation = document.querySelector(".sidenav");
 const header = document.querySelector("header");
 const main = document.querySelector("main");
 const footer = document.querySelector("footer");
-const sidenavClose = document.querySelector('#sidenav-close');
+const sidenavClose = document.querySelector("#sidenav-close");
 
-hamburgerMenu.addEventListener('click', function() {
-  navigation.classList.toggle('navigation--expanded');
+hamburgerMenu.addEventListener("click", function () {
+  navigation.classList.add("navigation--expanded");
 });
 
-sidenavClose.addEventListener('click', function() {
-  navigation.classList.toggle('navigation--expanded');
+sidenavClose.addEventListener("click", function () {
+  navigation.classList.remove("navigation--expanded");
 });
 
-const navigationLinks = document.querySelectorAll('.sidenav a');
+const navigationLinks = document.querySelectorAll(".sidenav a");
 
-navigationLinks.forEach(function(link) {
-    link.addEventListener('click', function(event) {
-      // Remove active class from all links
-      navigationLinks.forEach(function(link) {
-        link.classList.remove('active');
-      });
-  
-      // Add active class to clicked link
-      link.classList.add('active');
-
-      navigation.classList.toggle('navigation--expanded');
+navigationLinks.forEach(function (link) {
+  link.addEventListener("click", function (event) {
+    // Remove active class from all links
+    navigationLinks.forEach(function (link) {
+      link.classList.remove("active");
     });
+
+    // Add active class to clicked link
+    link.classList.add("active");
+
+    navigation.classList.remove("navigation--expanded");
   });
+});
